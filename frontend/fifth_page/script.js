1873556ebe27f739fe71dd01cfb804e6ffa0f6a8
Priyakaduar <priyakaduar2003@gmail.com>
2025-01-16 23:05:50 +0530

    Add files via upload

---
+++ frontend/fifth_page/script.js
@@ -0,0 +1,19 @@
+document.getElementsByName('Business')[0].addEventListener('click',function(){
+    window.location.href="/frontend/business/index.html";
+});
+
+document.getElementsByName('Ecommerce')[0].addEventListener('click',function(){
+    window.location.href="/frontend/ecommerce/index.html";
+});
+
+document.getElementsByName('Education')[0].addEventListener('click',function(){
+    window.location.href="/frontend/education/index.html";
+});
+
+document.getElementsByName('Blogging')[0].addEventListener('click',function(){
+    window.location.href="/frontend/blogging/index.html";
+});
+
+document.getElementsByName('TravelTourism')[0].addEventListener('click',function(){
+    window.location.href="/frontend/travel/index.html";
+});
